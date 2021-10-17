c40d775a5f41b04b7585ad12982f5cf4b73cf42e
Михайлов Евгений <jackmihailov@outlook.com>
2021-10-17 22:17:46 +0300

    Добавьте файлы проекта.

---
+++ HomeWork3.10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HomeWork3._10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+                Console.WriteLine("Введите число");
+                var num = int.Parse(Console.ReadLine());
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine($"{num} чётное");
+                }
+                else
+                {
+                    Console.WriteLine($"{num} не чётное");
+                }
+        }
+    }
+}
